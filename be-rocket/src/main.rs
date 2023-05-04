27b56117047468a4c7c4f73e09b7538133d50689
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2023-05-04 10:21:12 +0300

    store temp results

---
+++ be-rocket/src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
+
+// https://rocket.rs/v0.5-rc/guide/responses/#custom-responders
