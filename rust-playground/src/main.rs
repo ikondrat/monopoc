ed7742615fdf050c34eade45914f52f83de21154
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2023-05-06 20:13:23 +0300

    play with std

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -1,6 +1,5 @@
-fn main() {
-    println!("result: {}", gcd(14, 15));
-}
+use std::str::FromStr;
+use std::env;
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
@@ -24,5 +23,26 @@ fn test_gcd() {
                3 * 11);
 }
 
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
 
 
