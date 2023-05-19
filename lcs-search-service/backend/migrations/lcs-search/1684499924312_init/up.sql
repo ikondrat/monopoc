a6e74f4da2642ed71584c8497fad3ec186ba0f2f
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2023-05-19 14:39:21 +0200

    update migrations

--- lcs-search-service/backend/migrations/lcs-search/1684499924312_init/up.sql
+++ lcs-search-service/backend/migrations/lcs-search/1684499924312_init/up.sql
@@ -1,14 +1,14 @@
 SET check_function_bodies = false;
 CREATE TABLE public.places (
     uuid uuid DEFAULT gen_random_uuid() NOT NULL,
-    website text NOT NULL,
-    phone_number text NOT NULL,
+    website text,
+    phone_number text,
     name text NOT NULL,
-    address text,
+    address text NOT NULL,
     working_hours json
 );
 COMMENT ON TABLE public.places IS 'Contains places';
 ALTER TABLE ONLY public.places
-    ADD CONSTRAINT places_pkey PRIMARY KEY (uuid, name);
+    ADD CONSTRAINT places_pkey PRIMARY KEY (uuid, name, address);
 ALTER TABLE ONLY public.places
     ADD CONSTRAINT places_uuid_key UNIQUE (uuid);
