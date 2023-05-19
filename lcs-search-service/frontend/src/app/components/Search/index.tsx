f01deb54af1a22a1b35d5acdb26cfc3083dd94be
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2023-05-19 17:25:20 +0200

    fetch data

---
+++ lcs-search-service/frontend/src/app/components/Search/index.tsx
@@ -0,0 +1,15 @@
+import { gql, useQuery } from '@apollo/client'
+
+const SEARCH_PLACES = gql`
+  query searchByNameOrAddress($search: String!) {
+    places(where: { _or: [{ address: { _like: $search } }, { name: { _like: $search } }] }) {
+      name
+    }
+  }
+`
+export function Search({ value }: { value: string }) {
+  const { data } = useQuery(SEARCH_PLACES, {
+    variables: { search: `%${value}%` }
+  })
+  return <div>I am search: {data?.places.length}</div>
+}
